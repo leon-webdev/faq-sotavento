53063f112ef8e4a5849c4c6d599e96a706f2a0e2
Leon G. Martinez Ostos <leonma19@gmail.com>
2021-01-20 10:24:17 -0500

    add components header, footer and layout

---
+++ src/components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer">
+        <img
+          alt="Magenta and blue patterned design"
+          src="/assets/adorno3.png"
+          className="adorno__3"
+        />
+
+        <p className="footer__p">
+          Este sitio web es de uso exclusivo de los futuros propietarios del
+          megaproyecto sotavento arena - vento. la informacion suministrada acá
+          ha sido recopilada por los miembros del grupo.
+        </p>
+
+        <p
+          className="footer__p"
+          style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}
+        >
+          ↳ NO SOMOS ASESORES INMOBILIARIOS NI BANCARIOS.
+        </p>
+
+        <ul className="footer__sign">
+          <li>
+            Hecho con <span style={{color: "red"}}>♥</span> por
+            <a href="https://instagram.com/leon_webdev/" target="_blank" rel="noreferrer">
+              León
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <a href="/">
+          <div>
+            <span>FAQ Sotavento Arena - Vento</span>
+          </div>
+        </a>
+      </header>
+    </>
+  )
+}
+
+export default Header
