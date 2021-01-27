5f45dece7a8366fa2c8cc26a019bae8e981d00ab
Leon G. Martinez Ostos <leonma19@gmail.com>
2021-01-27 10:23:46 -0500

    add google analytics to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,20 @@ module.exports = {
     siteUrl: 'https://faq-sotavento.netlify.app',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-MKHN3ECWL9", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
